eaace03411a9a0d8bd66247b058cac4484680762
lijingyu <18743007098@139.com>
2017-06-28 23:58:50 +0800

    修改

--- src/com/lijy/user/service/DepartmentService.java
+++ src/com/lijy/user/service/DepartmentService.java
@@ -3,31 +3,30 @@ package com.lijy.user.service;
 import java.util.List;
 
 import com.lijy.user.dao.DepartmentDao;
-import com.lijy.user.exception.MyException;
 import com.lijy.user.model.DepartmentBean;
 
 /**
- * @description: 
+ * @description:
  * 
  * @author : lijy
  * @date : 2017-6-17
  */
 public class DepartmentService {
-	
+
 	public void addDepartment(DepartmentBean bean) {
 		DepartmentDao dao = new DepartmentDao();
-		if (bean.getDeptName() == null){
+		if (bean.getDeptName() == null) {
 			throw new NullPointerException("部门名称不能为空！");
 		}
 		dao.addDepartment(bean);
 	}
-	
-	public List<DepartmentBean> queryDepartmentList(){
+
+	public List<DepartmentBean> queryDepartmentList() {
 		DepartmentDao dao = new DepartmentDao();
 		return dao.queryDepartmentList();
 	}
-	
-	//删除部门
+
+	// 删除部门
 	public void deleteOne(String deptName) {
 		if (deptName == null || "".equals(deptName)) {
 			throw new NullPointerException("部门名称为空！");
@@ -35,6 +34,5 @@ public class DepartmentService {
 		DepartmentDao dao = new DepartmentDao();
 		dao.deleteOne(deptName);
 	}
-	
-}
 
+}
